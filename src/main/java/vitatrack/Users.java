0d0576a8af868287cdd99ba9022fd328e906e0de
Andy DiCarlo <andy01880@gmail.com>
2024-04-09 17:38:04 -0400

    Add html resources, add app.properties, change user to users so it does not give DB error when starting springboot

--- src/main/java/vitatrack/Users.java
+++ src/main/java/vitatrack/Users.java
@@ -1,7 +1,16 @@
 package vitatrack;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
